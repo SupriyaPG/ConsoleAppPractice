d084cb51d931cf27fc68c94a344345293467dd0d
SupriyaPG <supriyapg369@gmail.com>
2025-09-07 13:44:58 +0530

    Add project files.

---
+++ ConsoleAppPractice/function.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppPractice
+{
+    internal class function
+    {
+        internal static void fun()
+        {
+            int a = 10;
+            Console.WriteLine(a);
+        }
+
+
+        internal static void fun1()
+        {
+            int b = 100;
+            Console.WriteLine(b);
+        }
+    }
+
+}
